deada138821731ceff58c501d4122e494e9875ef
Alexandre de Lima <allima1991@gmail.com>
2020-08-22 17:36:40 -0400

    first commit

---
+++ src/main/java/com/ropalon/academia/forca/AcademiaForcaApplication.java
@@ -0,0 +1,13 @@
+package com.ropalon.academia.forca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaForcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaForcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ropalon/academia/forca/AcademiaForcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ropalon.academia.forca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaForcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

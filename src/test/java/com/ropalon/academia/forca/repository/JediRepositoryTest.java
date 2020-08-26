63e180cee1906907f0854051e29cbedf430be291
Alexandre de Lima <allima1991@gmail.com>
2020-08-25 23:07:14 -0300

    add teste controller

--- src/test/java/com/ropalon/academia/forca/repository/JediRepositoryTest.java
+++ src/test/java/com/ropalon/academia/forca/repository/JediRepositoryTest.java
@@ -1,16 +1,26 @@
 package com.ropalon.academia.forca.repository;
 
+import com.ropalon.academia.forca.model.Jedi;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class JediRepositoryTest {
+    @Autowired
+    private JediRepository controller;
 
     @Test
     void getAllJediComSucesso() {
+        assertThat(controller.getAllJedi()).isNotNull();
     }
 
     @Test
     void addComSucesso() {
+        controller.add(new Jedi());
+        Assertions.assertEquals(controller.getAllJedi().size(),2);
     }
 }

a74d8b1f93f976fe0a117a5e9f58bad4968b7930
Alexandre de Lima <allima1991@gmail.com>
2020-08-25 22:29:39 -0300

    Remove this unused import

--- src/main/java/com/ropalon/academia/forca/controller/JediController.java
+++ src/main/java/com/ropalon/academia/forca/controller/JediController.java
@@ -3,19 +3,15 @@ package com.ropalon.academia.forca.controller;
 import com.ropalon.academia.forca.model.Jedi;
 import com.ropalon.academia.forca.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 public class JediController {

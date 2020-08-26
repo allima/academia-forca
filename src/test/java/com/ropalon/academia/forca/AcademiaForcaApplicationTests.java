fb6a315ecda5ed87f17700e2d178d9ee0e637ce7
Alexandre de Lima <allima1991@gmail.com>
2020-08-25 22:45:52 -0300

    add teste controller

--- src/test/java/com/ropalon/academia/forca/AcademiaForcaApplicationTests.java
+++ src/test/java/com/ropalon/academia/forca/AcademiaForcaApplicationTests.java
@@ -1,14 +1,21 @@
 package com.ropalon.academia.forca;
 
+import com.ropalon.academia.forca.controller.JediController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class AcademiaForcaApplicationTests {
 
+	@Autowired
+	private JediController controller;
+
 	@Test
 	void contextLoads() {
-
+		assertThat(controller).isNotNull();
 	}
 
 }
